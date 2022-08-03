f97ca8661cdcfcd34e2c5bf9779e112db0a6b6dc
ChetanX <aremarati.chetan@gmail.com>
2022-08-03 17:18:30 +0530

    6th commit

--- src/calculator/Sub_method.java
+++ src/calculator/Sub_method.java
@@ -4,13 +4,14 @@ import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 public class Sub_method {
 
 	public static int x =10;
 	public static int y =4;
 	Calculator cal = new Calculator(x, y);
+	
 	@Test
 	public void verifysubMethod() {
 		System.out.println("");
@@ -26,9 +27,5 @@ public class Sub_method {
 		System.out.println("set up was done");
 	}
 	
-	@Before
-	public void method() {
-		System.out.println(" for local branch ");
-		
-	}
+
 }
